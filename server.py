05e1c7d14fb393c9e797ff5e4427e746e3208dc5
GalloGGMC <gabriel.coutinho@sou.inteli.edu.br>
2024-06-13 16:50:07 -0300

    add linear

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
-model = load_model("modelo_mnist.h5")
+
 
 @app.route('/')
 def home():
@@ -17,6 +17,10 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    model = None
+    model2 = None
+    model = load_model("model_flat.h5")
+    model2 = load_model("modelo_mnist.h5")
     a = request.files["image"]
     a.save("image.jpg")
     img = image.load_img("image.jpg", target_size=(28,28), color_mode="grayscale")
@@ -26,7 +30,8 @@ def upload():
     img[copy <= 128] = 255
     img = img/img.max()
     prediction = model.predict(img.reshape(1, 28, 28, 1))
-    return render_template("index.html", result=np.argmax(prediction))
+    prediction2 = model2.predict(img.reshape(1, 28, 28, 1))
+    return render_template("index.html", result1=np.argmax(prediction), result2=np.argmax(prediction2))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
